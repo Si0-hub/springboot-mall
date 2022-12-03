20b36b7c39bdcf9d2d188924bf782d55fb18c830
Si0-hub <oioi40209@gmail.com>
2022-12-03 10:52:40 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/john/springbootmall/controller/ProductController.java
+++ src/main/java/com/john/springbootmall/controller/ProductController.java
@@ -53,4 +53,11 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<Product> deleteProductById(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }

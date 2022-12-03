8687044714f0c9d0c471f9ca924eef90c99c67ae
Si0-hub <oioi40209@gmail.com>
2022-12-03 11:14:54 +0800

    商品功能 - 實作查詢商品列表 (基本回傳多項)

--- src/main/java/com/john/springbootmall/dao/ProductDao.java
+++ src/main/java/com/john/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.john.springbootmall.dao;
 import com.john.springbootmall.dto.ProductRequest;
 import com.john.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProdictById(Integer productId);
 
--- src/main/java/com/john/springbootmall/service/ProductService.java
+++ src/main/java/com/john/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.john.springbootmall.service;
 import com.john.springbootmall.dto.ProductRequest;
 import com.john.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 

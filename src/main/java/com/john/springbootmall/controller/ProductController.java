422afac3ec37ebeab71281f779c1535b7e9c0af9
Si0-hub <oioi40209@gmail.com>
2022-12-06 22:07:43 +0800

    商品功能 - 實作查詢商品列表 (查詢條件)

--- src/main/java/com/john/springbootmall/controller/ProductController.java
+++ src/main/java/com/john/springbootmall/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.john.springbootmall.controller;
 
+import com.john.springbootmall.constant.ProductCategory;
 import com.john.springbootmall.dto.ProductRequest;
 import com.john.springbootmall.model.Product;
 import com.john.springbootmall.service.ProductService;
@@ -18,8 +19,11 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts() {
-        List<Product> productList = productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(
+            @RequestParam(required = false) ProductCategory category,
+            @RequestParam(required = false) String search
+    ) {
+        List<Product> productList = productService.getProducts(category, search);
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- src/main/java/com/john/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/john/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,5 +1,6 @@
 package com.john.springbootmall.dao.impl;
 
+import com.john.springbootmall.constant.ProductCategory;
 import com.john.springbootmall.dao.ProductDao;
 import com.john.springbootmall.dto.ProductRequest;
 import com.john.springbootmall.model.Product;
@@ -20,10 +21,20 @@ public class ProductDaoImpl implements ProductDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
-    public List<Product> getProducts() {
-        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, created_date, last_modified_date FROM product";
+    public List<Product> getProducts(ProductCategory category, String search) {
+        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, created_date, last_modified_date FROM product WHERE 1=1";
 
         Map<String, Object> map = new HashMap<>();
+
+        if (category != null) {
+            sql = sql + " AND category = :category";
+            map.put("category", category.name());
+        }
+
+        if (search != null) {
+            sql = sql + " AND product_name LIKE :search";
+            map.put("search", "%" + search + "%");
+        }
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 

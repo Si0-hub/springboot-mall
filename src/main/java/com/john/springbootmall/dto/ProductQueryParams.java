cdf77b41c95f6d318e8a2b95d40e566314f836d6
Si0-hub <oioi40209@gmail.com>
2022-12-12 11:31:21 +0800

    商品功能 - 實作查詢列表 (分頁)

--- src/main/java/com/john/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/john/springbootmall/dto/ProductQueryParams.java
@@ -9,6 +9,8 @@ public class ProductQueryParams {
     private String search;
     private String orderBy;
     private String sort;
+    private Integer limt;
+    private Integer offset;
 
     public ProductCategory getCategory() {
         return category;
@@ -40,5 +42,21 @@ public class ProductQueryParams {
 
     public void setSort(String sort) {
         this.sort = sort;
+    }
+
+    public Integer getLimt() {
+        return limt;
+    }
+
+    public void setLimt(Integer limt) {
+        this.limt = limt;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
     }
 }

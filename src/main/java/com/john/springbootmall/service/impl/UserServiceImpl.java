6721ba25f32be6e8819f24a80e54202ab74ef317
Si0-hub <oioi40209@gmail.com>
2022-12-18 17:56:14 +0800

    帳號功能 - 登入

--- src/main/java/com/john/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/john/springbootmall/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.john.springbootmall.service.impl;
 
 import com.john.springbootmall.dao.UserDao;
+import com.john.springbootmall.dto.UserLoginRequest;
 import com.john.springbootmall.dto.UserRegisterRequest;
 import com.john.springbootmall.model.User;
 import com.john.springbootmall.service.UserService;
@@ -35,5 +36,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserById(Integer userId) {
         return userDao.getUserById(userId);
+    }
+
+    @Override
+    public User login(UserLoginRequest userLoginRequest) {
+        User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+
+        if (user == null) {
+            log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+            return user;
+        } else {
+            log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
